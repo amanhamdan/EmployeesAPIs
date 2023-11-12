5a3d9af3a78be1f1593ffeb997f881f6b1ba11a1
amanhamdan <aman.hamdan@contractoralefeducation.com>
2023-11-13 03:53:19 +0400

    first commit

---
+++ src/main/java/com/example/TestEmployee/TestEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestEmployeeApplication.class, args);
+	}
+
+}
